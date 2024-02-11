2aaf1c5aa3da2e92cdbc3c2fe11f13aca2dab75a
Renato <freitas.camapum@gmail.com>
2024-02-11 19:47:37 +0000

    intro fixed

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -8,7 +8,7 @@ const Intro = () => {
         <div className='intro-content'>
             <span className='hello'>Hello,</span>
             <span className='intro-text'>I'm <span className='intro-name'>Renato</span> <br/>web developer.</span>
-            <p className='intro-paragraph'>Lorem ipsum dolor sit amet, <br/>consectetur adipiscing elit, sed do eiusmod tempor incididunt </p>
+            <p className='intro-paragraph'>Commited web developer passionate about crafting elegant solutions. Dedicated to enhancing user experiences through innovative design and seamless functionality</p>
             <a href='./Renato-cv.pdf' download><button className='cv-btn'><i class="fa-solid fa-file-arrow-down"></i>Download CV</button></a>
         </div>
         <img src={picture} alt='' className='bg-picture' />
