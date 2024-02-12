05d139fcd4abc513f15c6e66cbd1ee0b7c0c6a5f
Renato <freitas.camapum@gmail.com>
2024-02-12 23:45:53 +0000

    work added

--- src/components/Works/works.js
+++ src/components/Works/works.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './works.css'
 import memory from '../../assets/memory-game.png'
-import huddle from '../../assets/huddle.png'
+import tindog from '../../assets/tindogs.png'
 import landing from '../../assets/landing-page.png'
 
 const Works = () => {
@@ -11,7 +11,7 @@ const Works = () => {
        <sapn className="works-description">Explore my portfolio for a glimpse into my web development journey. Discover diverse projects showcasing creativity, and user-centric design.</sapn>
        <div className="works-images">
         <a href='https://tilt-memory-game.netlify.app/' target='_blank' rel="noreferrer"><img src={memory} alt="" className="works-image" /></a>
-        <a href='https://renato-huddle-landing-page.netlify.app/' target='_blank' rel="noreferrer"><img src={huddle} alt="" className="works-image" /></a>
+        <a href='https://renato-bootstrap.netlify.app/' target='_blank' rel="noreferrer"><img src={tindog} alt="" className="works-image" /></a>
         <a href='https://renato-landing-page.netlify.app/' target='_blank' rel="noreferrer"><img src={landing} alt="" className="works-image" /></a>
        </div>
        <btn className="works-btn"><a href='https://github.com/Renato-Camapum' target='_blank'  rel="noreferrer">See More</a></btn>
